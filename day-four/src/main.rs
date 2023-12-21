aaf6d9bc55f2ce4ffc509ff38151bff8c42a62cf
Austin Allman <professorallman@gmail.com>
2023-12-21 00:54:46 -0600

    day 4 part 2 work in progress

--- day-four/src/main.rs
+++ day-four/src/main.rs
@@ -1,11 +1,12 @@
 
 use std::fs::File;
 use std::io::{BufReader, BufRead};
+use std::ops::Add;
 
 fn main() -> std::io::Result<()> {
     let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
-    let winning_number_totals = reader
+    let data = reader
         .lines()
         .into_iter()
         .map(|line| {
@@ -28,14 +29,17 @@ fn main() -> std::io::Result<()> {
                 .filter(|line| line.parse::<i64>().is_ok())
                 .map(|part| part.parse::<i64>().unwrap())
                 .collect::<Vec<_>>();
-            println!("{},{:?},{:?}", round, winning_numbers, your_numbers);
 
             (round.to_owned(), winning_numbers.to_owned(), your_numbers.to_owned())
-        })
-        .map(|(_,winning_numbers, your_numbers)|{
+        });
+        let mut games:Vec<Game> = Vec::new();
+        let winning_number_totals = data.map(|(_,winning_numbers, your_numbers)|{
             let mut points = 0;
+            let mut number_of_dubs = 0;
+
             your_numbers.iter()
                 .filter(|number| winning_numbers.contains(number))
+                .inspect(|_| number_of_dubs = number_of_dubs + 1)
                 .for_each(|_|{ if points == 0 {
                     points = 1;
                 }else{
@@ -43,13 +47,29 @@ fn main() -> std::io::Result<()> {
                 }
                 });
 
+            games.push(Game { instances: 0, number_of_dubs });
+
             points
         })
         .reduce(|a, b| a + b).unwrap();
     println!("The answer in: {:?}", winning_number_totals);
+    for  i in 0..games.len(){
+        play_game(&mut games, i);
+    }
+    println!("The games:\n{:?}", games);
     Ok(())
-
-//    for line in reader.lines(){
-
-    //}
+}
+#[derive(Debug)]
+struct Game{
+    instances:i32,
+    number_of_dubs:i32
+}
+fn play_game(games:&mut Vec<Game>, i:usize){
+    let game = games.get_mut(i).unwrap();
+    game.instances = game.instances + 1;
+    let number_of_dubs:usize = game.number_of_dubs.try_into().expect("should work");
+    println!("Playing game {}, num dubs:{}", i, number_of_dubs);
+    for j in i.add(1)..number_of_dubs.add(1){
+        play_game(games, j);
+    }
 }
